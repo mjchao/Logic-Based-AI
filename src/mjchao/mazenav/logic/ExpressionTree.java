d3b9cc7a31cff2bc928ac97ec6db77a649a20ee7
Mickey Chao <mjchao2@gmail.com>
2016-04-14 19:36:25 -0400

    Fix problem with standardize scope resolution

--- src/mjchao/mazenav/logic/ExpressionTree.java
+++ src/mjchao/mazenav/logic/ExpressionTree.java
@@ -481,13 +481,26 @@ class ExpressionTree {
 	public void convertToCNF( SymbolTracker tracker ) {
 		if ( !inCNF ) {
 			buildTree();
+			System.out.println( toPostfix( tracker ) );
 			eliminateArrowsAndDistributeNots();
+			System.out.println( toPostfix( tracker ) );
 			standardize( tracker );
+			System.out.println( toPostfix( tracker ) );
 			skolemize( tracker );
+			System.out.println( toPostfix( tracker ) );
 			dropQuantifiers();
+			System.out.println( toPostfix( tracker ) );
 			distributeOrOverAnd();
 			inCNF = true;
 		}
+	}
+	
+	private ArrayList< Symbol > toPostfix( SymbolTracker tracker ) {
+		ArrayList< Symbol > rtn = new ArrayList< Symbol >();
+		if ( root != null ) {
+			root.buildPostfix( rtn );
+		}
+		return rtn;		
 	}
 	
 	/**
@@ -497,11 +510,7 @@ class ExpressionTree {
 	 */
 	public List< Symbol > getCNFPostfix( SymbolTracker tracker ) {
 		convertToCNF( tracker );
-		ArrayList< Symbol > rtn = new ArrayList< Symbol >();
-		if ( root != null ) {
-			root.buildPostfix( rtn );
-		}
-		return rtn;
+		return toPostfix( tracker );
 	}
 	
 	class ExpressionNode {
@@ -688,8 +697,10 @@ class ExpressionTree {
 					child.parent = null;
 				}
 				else {
-					this.parent.getChildren().clear();
-					this.parent.addChildren( child );
+					//this.parent.getChildren().clear();
+					//this.parent.addChildren( child );
+					this.parent.getChildren().set( 0 , child );
+					child.parent = this.parent;
 				}
 				
 				//negate the child if necessary
@@ -700,6 +711,7 @@ class ExpressionTree {
 				child.distributeNots();
 			}
 			else {
+				
 				for ( ExpressionNode child : children ) {
 					child.distributeNots();
 				}
@@ -715,6 +727,7 @@ class ExpressionTree {
 		 */
 		public void standardize( HashMap< Variable , Variable > userSystemMapping , SymbolTracker tracker ) {
 			if ( this.getValue() instanceof Variable ) {
+				Variable oldMapping = userSystemMapping.get( this.getValue() );
 				if ( userSystemMapping.get( this.getValue() ) == null ) {
 					
 					//if no mapping for the user-defined variable 
@@ -732,6 +745,8 @@ class ExpressionTree {
 				for ( ExpressionNode child : this.getChildren() ) {
 					child.standardize( userSystemMapping , tracker );
 				}
+				
+				userSystemMapping.put( (Variable)this.getValue() , oldMapping );
 			}
 			else if ( this.getValue() instanceof QuantifierList ) {
 				
@@ -746,7 +761,7 @@ class ExpressionTree {
 					//to be restored when this quantifier goes out of scope
 					oldVariables.add( v );
 					oldMappings.add( userSystemMapping.get( v ) );
-					
+
 					userSystemMapping.put( v , tracker.getNewSystemVariable() );
 					((QuantifierList) this.getValue()).standardizeVariable( v , userSystemMapping.get( v ) );
 				}
